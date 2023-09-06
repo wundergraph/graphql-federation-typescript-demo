314a43772c0ca6f6d29e466892cd150866f75cbe
fiam <alberto@garciahierro.com>
2023-09-06 18:17:24 +0100

    read schema from schema.graphql

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,20 @@
 // packages (http, express, graphql, etc...) are loaded
 import "./telemetry";
 
+import fs from "fs";
+
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { IncomingMessage } from "http";
+import path from "path";
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
+const typeDefs = fs.readFileSync(
+  path.join(__dirname, "schema.graphql"),
+  "utf8"
+);
 
 const books = [
   {
